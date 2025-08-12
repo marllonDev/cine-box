3df088e6d7f257b7356b7ac29e7261541299ce03
marllonDev <marlonzucolotto@gmail.com>
2025-08-12 17:46:18 -0300

    Subindo projeto com imagens

---
+++ cine-box-front/lib/cinebox_main_app.dart
@@ -0,0 +1,17 @@
+import 'package:cinebox/ui/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CineboxMainApp extends StatelessWidget {
+  const CineboxMainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cinebox',
+      routes: {
+        '/': (context) => const SplashScreen(),
+      },
+    );
+  }
+}
---
+++ cine-box-front/lib/ui/splash/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:cinebox/config/env.dart';
+import 'package:cinebox/ui/core/themes/resource.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
+      ),
+    );
+  }
+}
